c9212f1bf5cfe50de87227e1e54035dd832b0a1e
Dark <djcabrera@sudamericano.edu.ec>
2026-04-08 16:31:33 -0500

    Commit #09

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { InteraccionModule } from './interaccion/interaccion.module';
 import { MatchModule } from './match/match.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [PrismaModule, UsuarioModule, InteraccionModule, MatchModule],
+  imports: [PrismaModule, UsuarioModule, InteraccionModule, MatchModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
