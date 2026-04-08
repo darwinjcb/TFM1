143aac9a59586ea61a6a10b4b6918c3965f38a6e
Dark <djcabrera@sudamericano.edu.ec>
2026-04-08 12:16:14 -0500

    Commit #04

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,8 @@
+// src/prisma/prisma.module.ts:
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService]
+})
+export class PrismaModule { }
