96f5ce5166ea3eee1d02f4f53e68f82ea6f39099
Dark <djcabrera@sudamericano.edu.ec>
2026-04-11 11:05:22 -0500

    Commit #25

--- src/usuario/dominio/usuario-repositorio.ts
+++ src/usuario/dominio/usuario-repositorio.ts
@@ -1,2 +1,57 @@
 // src/usuario/dominio/usuario-repositorio.ts:
-export class UsuarioRepositorio { }
+import { Usuario } from './usuario';
+
+export const USUARIO_REPOSITORIO = 'USUARIO_REPOSITORIO';
+
+export interface CrearUsuarioDatos {
+    nombre: string;
+    edad: number;
+    biografia?: string;
+    peso?: number;
+    altura?: number;
+    nacionalidad?: string;
+    genero: string;
+    ciudad?: string;
+    pais?: string;
+    numero?: string;
+    correo: string;
+    signoZodiacal?: string;
+    queBusca?: string;
+    ubicacion?: string;
+    hobbie?: string;
+    profesion?: string;
+    fotos?: string[];
+    estaActivo?: boolean;
+}
+
+export interface ActualizarUsuarioDatos {
+    nombre?: string;
+    edad?: number;
+    biografia?: string;
+    peso?: number;
+    altura?: number;
+    nacionalidad?: string;
+    genero?: string;
+    ciudad?: string;
+    pais?: string;
+    numero?: string;
+    correo?: string;
+    signoZodiacal?: string;
+    queBusca?: string;
+    ubicacion?: string;
+    hobbie?: string;
+    profesion?: string;
+    fotos?: string[];
+    estaActivo?: boolean;
+}
+
+export interface UsuarioRepositorio {
+    crear(datos: CrearUsuarioDatos): Promise<Usuario>;
+    obtenerTodos(): Promise<Usuario[]>;
+    obtenerPorId(idUsuario: number): Promise<Usuario | null>;
+    actualizar(
+        idUsuario: number,
+        datos: ActualizarUsuarioDatos,
+    ): Promise<Usuario>;
+    eliminar(idUsuario: number): Promise<Usuario>;
+}
--- src/usuario/dominio/usuario.ts
+++ src/usuario/dominio/usuario.ts
@@ -1,2 +1,26 @@
 // src/usuario/dominio/usuario.ts:
-export class Usuario { }
+export class Usuario {
+    constructor(
+        public readonly idUsuario: number,
+        public readonly nombre: string,
+        public readonly edad: number,
+        public readonly biografia: string | null,
+        public readonly peso: number | null,
+        public readonly altura: number | null,
+        public readonly nacionalidad: string | null,
+        public readonly genero: string,
+        public readonly ciudad: string | null,
+        public readonly pais: string | null,
+        public readonly numero: string | null,
+        public readonly correo: string,
+        public readonly signoZodiacal: string | null,
+        public readonly queBusca: string | null,
+        public readonly ubicacion: string | null,
+        public readonly hobbie: string | null,
+        public readonly profesion: string | null,
+        public readonly fotos: string[],
+        public readonly estaActivo: boolean,
+        public readonly fechaCreacion: Date,
+        public readonly fechaActualizacion: Date,
+    ) { }
+}
