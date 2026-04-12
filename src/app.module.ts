1c3aaf5e3483960e253be1ecd99caea65dcbd554
Dark <djcabrera@sudamericano.edu.ec>
2026-04-12 01:46:13 -0500

    Commit #03
    Módulo y Servicio de Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './infraestructura/usuario/usuario.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule { }
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,5 @@
+// src/prisma/prisma.service.ts:
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService { }
