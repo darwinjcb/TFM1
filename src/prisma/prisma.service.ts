4c906eddf1b26ad88015c11bbc7a8d50d824a562
Dark <djcabrera@sudamericano.edu.ec>
2026-04-13 23:44:19 -0500

    Commit #04

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,19 @@
 // src/prisma/prisma.service.ts:
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
-export class PrismaService { }
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL,
+        });
+
+        super({ adapter });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+}
